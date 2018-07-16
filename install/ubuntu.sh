95b1cddfb8219ce23dd1fbce5720e6b6b50daaeb
Ares Meroueh <ares.m@asidatascience.com>
2018-07-16 15:24:49 +0000

    Add neovim installation

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -2,3 +2,10 @@ wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
 echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
 sudo apt-get update
 sudo apt-get install rcm
+
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:neovim-ppa/stable -y
+sudo apt-get update -y
+sudo apt-get install neovim -y
+
+pip install neovim
