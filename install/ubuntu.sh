0a224d3a821d4e1d10d09116dfef2abcb255a7f9
Ares Meroueh <ares.m@asidatascience.com>
2018-11-04 09:56:37 +0000

    Test for nvim existence

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -1,13 +1,18 @@
+echo "Installing rcm..."
 wget -qO - https://apt.thoughtbot.com/thoughtbot.gpg.key | sudo apt-key add -
 echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
 sudo apt-get update
 sudo apt-get install rcm
 
-sudo apt-get install software-properties-common -y
-sudo apt-add-repository ppa:neovim-ppa/stable -y
-sudo apt-get update -y
-sudo apt-get install neovim -y
+if [ -z $( command -v nvim ) ]; 
+then
+	echo "Installing neovim..."
+	sudo apt-get install software-properties-common -y
+	sudo apt-add-repository ppa:neovim-ppa/stable -y
+	sudo apt-get update -y
+	sudo apt-get install neovim -y
+	pip install neovim
+fi
 
-pip install neovim
 pip install flake8
 pip install black
